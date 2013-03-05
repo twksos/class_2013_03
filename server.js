e7780571fb0eebc3f3cb407524a5737271172ec3
William Wei <twksos@qq.com>
2013-03-05 10:03:24 -0500

    set validation for weixin platform

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var http = require('http');
+var url = require("url");
+var crypto = require('crypto');
+var sha1 = crypto.createHash('sha1');
+var token = 'token';
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        var queries = url.parse(req.url,true).query
+        var text = token + queries.timestamp + queries.nonce;
+        if(sha1.update(text) === queries.signature)
+          res.end(queries.echostr);
+        else 
+          res.end('Hello World\n'); 
 }).listen(process.env.PORT);
