c14fd029080a51785177c4729881ddf47f8ef761
William Wei <twksos@qq.com>
2013-03-06 04:13:50 -0500

    simsimi

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 var express = require('express');
-var url = require("url");
 var crypto = require('crypto');
 var token = 'token';
+var http = require('http');
 
+function simsimi(me,sender,msg_type,text,res) {
+    var return_msg = '不好意思，我只认字。';
+    if (text) {        
+        var options = {
+            host: 'api.simsimi.com',
+            path: '/request.p?key=ab3c48c2-1d95-4266-92f1-e1436aab9b18&text=' + text + '&lc=ch'
+        };
+        var return_msg = '糟糕。。。出错了。';
+        http.get(options, function(simsimi_res) {
+            var reply = '';
+            simsimi_res.addListener('data', function(data) {
+                reply += data
+            });
+            simsimi_res.addListener('end', function(data) {
+                eval('var json= ' + reply);
+                if (json.result == 100) return_msg =  json.response;
+                
+                var response = '<xml>' + '<ToUserName><![CDATA[' + sender + ']]></ToUserName>' + '<FromUserName><![CDATA[' + me + ']]></FromUserName>' + '<CreateTime>' + Date.now() + '</CreateTime>' + '<MsgType><![CDATA[text]]></MsgType>' + '<Content><![CDATA[' + return_msg + ']]></Content>' + '<FuncFlag>0</FuncFlag>' + '</xml>';
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(response);
+            });
+        }).on('error', function(e) {
+            console.log('ERROR: ' + e.message);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+        });        
+    }
+}
+
+function normal_response(me, sender, msg_type, text, res) {
+    var return_msg = '不好意思，我只认字。';
+    if (text) {
+        return_msg = '你好，你刚才说：\n' + text + '\n对吧';
+    }
+    var response = '<xml>' + '<ToUserName><![CDATA[' + sender + ']]></ToUserName>' + '<FromUserName><![CDATA[' + me + ']]></FromUserName>' + '<CreateTime>' + Date.now() + '</CreateTime>' + '<MsgType><![CDATA[text]]></MsgType>' + '<Content><![CDATA[' + return_msg + ']]></Content>' + '<FuncFlag>0</FuncFlag>' + '</xml>';
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end(response);
+}
 var app = express.createServer(express.logger())
 app.use(express.bodyParser());
 app.get('/', function(req, res) {
-    var queries = url.parse(req.url, true).query
-    var content = validate_query_from_weixin(queries, token);
+    var content = validate_query_from_weixin(req.query, token);
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
@@ -24,27 +62,23 @@ app.post('/', function(req, res) {
         var me = match_var(/<ToUserName><!\[CDATA\[(.*?)\]\]><\/ToUserName>/, xml);
         var sender = match_var(/<FromUserName><!\[CDATA\[(.*?)\]\]><\/FromUserName>/, xml);
         var msg_type = match_var(/<MsgType><!\[CDATA\[(.*?)\]\]><\/MsgType>/, xml);
+            
+        
         if (sender !== null && me !== null) {
-            var return_msg = '不好意思，我只认字。';
             if (msg_type === 'text') {
                 var text = match_var(/<Content><!\[CDATA\[(.*?)\]\]><\/Content>/, xml);
-                return_msg = '你好，你刚才说：\n' + text + '\n对吧';
+                simsimi(me,sender,msg_type,text,res);
+                //normal_response(me,sender,msg_type,text,res);
             }
-            
-            var response = '<xml>' + '<ToUserName><![CDATA[' + sender + ']]></ToUserName>' + '<FromUserName><![CDATA[' + me + ']]></FromUserName>' + '<CreateTime>' + Date.now() + '</CreateTime>' + '<MsgType><![CDATA[text]]></MsgType>' + '<Content><![CDATA[' + return_msg + ']]></Content>' + '<FuncFlag>0</FuncFlag>' + '</xml>';
-
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.end(response);
         }
     });
 });
 
 var port = process.env.PORT || 80
 app.listen(port);
-function match_var(regex, text){
-    if (regex.test(text)){
+
+function match_var(regex, text) {
+    if (regex.test(text)) {
         return text.match(regex)[1];
     }
 }
